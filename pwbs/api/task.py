4591e94cca302ab5d98d105d6a3e11815519ec8c
Patryk Adamczyk <patryk.adamczyk@eengine.pl>
2019-12-18 14:31:47 +0100

    Task Base Class

---
+++ pwbs/api/task.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""PAiP Web Build System - Task Base Class
+
+NAME - PAiP Web Build System
+AUTHOR - Patryk Adamczyk <patrykadamczyk@paipweb.com>
+LICENSE - MIT
+
+"""
+# Imports
+from abc import ABC
+from abc import abstractmethod
+
+# Underscore Variables
+
+"""Author of the module"""
+__author__ = 'Patryk Adamczyk'
+"""Module License"""
+__license__ = 'MIT'
+"""Documentation format"""
+__docformat__ = 'restructuredtext en'
+
+
+# Task Base Class
+
+
+class Task(ABC):
+    """Task Base Class"""
+    @abstractmethod
+    def __init__(self):
+        """
+        Task Initialization Method
+        """
+        raise NotImplementedError('Not implemented Task Initialization Method')
+
+    @abstractmethod
+    def __call__(self, context=None):
+        """
+        Task Execution Method
+        :arg context: Context of Task Execution
+        """
+        raise NotImplementedError('Not implemented Task Execution Method')
